5ae820c496e7c7ea24defd5d071064476ddbb48b
Gene Laisne <glaisne@gmail.com>
2014-04-24 10:18:07 -0400

    Added some better Verbose output to Rename-DistributionGroup.ps1.

--- Rename-DistributionGroup.ps1
+++ Rename-DistributionGroup.ps1
@@ -36,42 +36,81 @@
 		[Boolean] $Confirm = $True
 	)
         
-        Get-DistributionGroup $OldName |fl * | out-string -stream | select-string $OldName
-        $group = Get-DistributionGroup $OldName
+    write-verbose "Fields that contain ""$OldName"""
+    try
+    {
+        Get-DistributionGroup $OldName -ErrorAction "Stop" |fl * | out-string -stream | select-string $OldName | 
+        Where {$_ -ne "" } | ForEach { write-verbose $_ }
+    }
+    catch [System.Management.Automation.RuntimeException]
+    {
+        Write-Verbose "There was an error accessing Distribution Group $OldName"
+	    write-host ("Exception: " + $Error[0].Exception.GetType().FullName)
+	    write-host $Error[0].Exception.Message
+        return
+    }
+    catch
+    {
+        Write-Verbose "There was an error accessing Distribution Group $OldName"
+        throw $_
+    }
+            
 
-        if ( $PSCmdlet.ShouldProcess($OldName, "Setting Name, Alias and DisplayName to $NewName") ) 
-		{
-			if ( -Not $Confirm -or $($PSCmdlet.ShouldContinue("Are you sure you want to make this change?","Setting Name, Alias and DisplayName to $NewName on target $OldName"  )) )
-            {
-                Set-DistributionGroup $OldName -alias $NewName -DisplayName $NewName -ForceUpgrade 
-            }
-        }
+    Write-Verbose "Getting the Distribution Group $OldName"
+    $group = Get-DistributionGroup $OldName
 
-        $group.EmailAddresses
-
-        foreach ($EmailAddress in $($group.EmailAddresses))
+    if ( $PSCmdlet.ShouldProcess($OldName, "Setting Name, Alias and DisplayName to $NewName") ) 
+	{
+		if ( -Not $Confirm -or $($PSCmdlet.ShouldContinue("Are you sure you want to make this change?","Setting Name, Alias and DisplayName to $NewName on target $OldName"  )) )
         {
-            if ($EmailAddress -notmatch "^smtp:.*@.*$")
-            { Continue }
-
-            $domain = $EmailAddress.split('@')[1]
-
-            $group.EmailAddresses += "smtp:$NewName@$Domain"
+            Set-DistributionGroup $OldName -alias $NewName -DisplayName $NewName -ForceUpgrade 
         }
-        Get-DistributionGroup $NewName |fl * | out-string -stream | select-string $OldName
-		if ( $PSCmdlet.ShouldProcess($OldName, "Adding new alias to the old address' smtp addresses.") ) 
-		{
+    }
 
-			if ( -Not $Confirm -or $($PSCmdlet.ShouldContinue("Are you sure you want to make this change?","Adding $NewName addresses on target $OldName"  )) )
+    Write-Verbose "EmailAddresses listed on the original group:"
+    $group.EmailAddresses |ft -auto | out-string -stream | Where {$_ -ne "" } | ForEach { write-verbose $_ }
+
+    foreach ($EmailAddress in $($group.EmailAddresses))
+    {
+        if ($EmailAddress -notmatch "^smtp:.*@.*$")
+        {
+            Write-Verbose "Skipping processing of EmailAddress $EmailAddress"
+            Continue
+        }
+
+        $domain = $EmailAddress.split('@')[1]
+
+        Write-Verbose "Current Emailaddress ($EmailAddress) domain = $Domain"
+
+        Write-Verbose "Adding new smtp address (smtp:$NewName@$Domain) to the EmailAddresses field"
+        $group.EmailAddresses += "smtp:$NewName@$Domain"
+    }
+        
+    Get-DistributionGroup $OldName -ErrorAction "Stop" |fl * | out-string -stream | select-string $OldName | 
+    Where {$_ -ne "" } | ForEach { write-verbose $_ }
+
+	if ( $PSCmdlet.ShouldProcess($OldName, "Adding new alias to the old address' smtp addresses.") ) 
+	{
+
+		if ( -Not $Confirm -or $($PSCmdlet.ShouldContinue("Are you sure you want to make this change?","Adding $NewName addresses on target $OldName"  )) )
+        {
+            if ($($group.EmailAddressPolicyEnabled))
             {
-
+                Write-Verbose "Turning off the Email Address Policy for this distribution group."
                 Set-DistributionGroup $NewName -EmailAddressPolicyEnabled:$false
-                Set-DistributionGroup $NewName -EmailAddresses $($group.EmailAddresses)
+            }
+            Write-Verbose "Setting the distribution groups new EmailAddresses setting."
+            Set-DistributionGroup $NewName -EmailAddresses $($group.EmailAddresses)
+                
+            if ($($group.EmailAddressPolicyEnabled))
+            {
+                Write-Verbose "Turning Back on the Email Address Policy for this distribution group."
                 Set-DistributionGroup $NewName -EmailAddressPolicyEnabled:$True
             }
         }
+    }
 
-        get-distributiongroup $NewName |fl emailaddresses
-        (get-distributiongroup $NewName).emailaddresses
+    Write-Verbose "New EmailAddresses listed on the original group:"
+    (get-distributiongroup $NewName).EmailAddresses | out-string -stream | Where {$_ -ne "" } | ForEach { write-verbose $_ }
 
 }
